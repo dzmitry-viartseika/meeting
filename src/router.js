30e3e1a4831e310fcf55516feab593d614428104
Dmitry Verteyko <verteyko1990@gmail.com>
2019-04-03 20:32:19 +0300

    '03.04.2019'

--- src/router.js
+++ src/router.js
@@ -17,43 +17,130 @@ export default new Router({
       path: '/',
       name: 'home',
       component: Home,
+      meta: {
+        title: 'Home Page - DevMeetup App',
+        metaTags: [
+          {
+            name: 'Some information about this app',
+            content: 'The home page of our example app.'
+          },
+          {
+            property: 'og:description',
+            content: 'The home page of our example app.'
+          }
+        ]
+      }
     },
     {
       path: '/meetup/new',
       name: 'CreateMeetup',
       component: CreateMeetup,
+      meta: {
+        title: 'Create Page - DevMeetup App',
+        metaTags: [
+          {
+            name: 'Some information about this app',
+            content: 'The home page of our example app.'
+          },
+          {
+            property: 'og:description',
+            content: 'The home page of our example app.'
+          }
+        ]
+      }
     },
     {
       path: '/meetups',
       name: 'Meetups',
       component: Meetups,
-    },
-    {
-      path: '/',
-      name: 'home',
-      component: Home,
+      meta: {
+        title: 'Meetups Page - DevMeetup App',
+        metaTags: [
+          {
+            name: 'Some information about this app',
+            content: 'The home page of our example app.'
+          },
+          {
+            property: 'og:description',
+            content: 'The home page of our example app.'
+          }
+        ]
+      }
     },
     {
       path: '/profile',
       name: 'Profile',
       component: Profile,
+      meta: {
+        title: 'Profile Page - DevMeetup App',
+        metaTags: [
+          {
+            name: 'Some information about this app',
+            content: 'The home page of our example app.'
+          },
+          {
+            property: 'og:description',
+            content: 'The home page of our example app.'
+          }
+        ]
+      }
     },
     {
       path: '/signin',
       name: 'Signin',
       component: Signin,
+      meta: {
+        title: 'Sign In Page  - DevMeetup App',
+        metaTags: [
+          {
+            name: 'Some information about this app',
+            content: 'The home page of our example app.'
+          },
+          {
+            property: 'og:description',
+            content: 'The home page of our example app.'
+          }
+        ]
+      }
     },
     {
       path: '/signup',
       name: 'Signup',
       component: Signup,
+      meta: {
+        title: 'Sign Up Page  - DevMeetup App',
+        metaTags: [
+          {
+            name: 'Some information about this app',
+            content: 'The home page of our example app.'
+          },
+          {
+            property: 'og:description',
+            content: 'The home page of our example app.'
+          }
+        ]
+      }
     },
     {
       path: '/meetup/:id',
       name: 'Meetup',
       component: Meetup,
-      props: true
+      props: true,
+      meta: {
+        title: 'Meetup Page  - DevMeetup App',
+        metaTags: [
+          {
+            name: 'Some information about this app',
+            content: 'The home page of our example app.'
+          },
+          {
+            property: 'og:description',
+            content: 'The home page of our example app.'
+          }
+        ]
+      }
     }
   ],
   mode: 'history'
 });
+
